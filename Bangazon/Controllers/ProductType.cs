b3cdc135813e8667512ae1fdc521fd7cca1fb805
Kayla Reid <iamkaylareid@gmail.com>
2018-10-29 16:07:43 -0500

    almost done

---
+++ Bangazon/Controllers/ProductType.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Bangazon.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bangazon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypeController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public ProductTypeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<IActionResult> Get(string q)
+        {
+            string sql = @"
+            SELECT 
+                p.Id,
+                p.Name
+                FROM ProductType p
+            WHERE 1=1
+        ";
+
+            using (IDbConnection conn = Connection)
+            {
+                IEnumerable<ProductType> productType = await conn.QueryAsync<ProductType>(
+                    sql);
+                return Ok(productType);
+            }
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}", Name = "GetProductType")]
+        public async Task<IActionResult> Get([FromRoute]int id)
+        {
+            string sql = $@"        
+                SELECT 
+                    p.Id,
+                    p.Name
+                FROM ProductType p
+                WHERE p.Id = {id}
+            ";
+
+            using (IDbConnection conn = Connection)
+            {
+                IEnumerable<ProductType> productType = await conn.QueryAsync<ProductType>(sql);
+                return Ok(productType.Single());
+            }
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ProductType productType)
+        {
+            string sql = $@"INSERT INTO ProductType(Name)
+            VALUES('{productType.Name}')
+            SELECT SCOPE_IDENTITY();";
+
+            using (IDbConnection conn = Connection)
+            {
+                var newId = (await conn.QueryAsync<int>(sql)).Single();
+                productType.Id = newId;
+                return CreatedAtRoute("GetProductType", new { id = newId }, productType);
+            }
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] ProductType productType)
+        {
+            string sql = $@"
+            UPDATE ProductType
+            SET Name = '{productType.Name}'
+            WHERE Id = {id}";
+
+            try
+            {
+                using (IDbConnection conn = Connection)
+                {
+                    int rowsAffected = await conn.ExecuteAsync(sql);
+                    if (rowsAffected > 0)
+                    {
+                       return new StatusCodeResult(StatusCodes.Status200OK);
+                    }
+
+                    throw new Exception("No rows affected");
+                }
+            }
+            catch (Exception)
+            {
+                if (!ProductTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+
+        private bool ProductTypeExists(int id)
+        {
+            string sql = $"SELECT Id FROM ProductType WHERE Id = {id}";
+            using (IDbConnection conn = Connection)
+            {
+                return conn.Query<ProductType>(sql).Count() > 0;
+            }
+        }
+    }
+}
