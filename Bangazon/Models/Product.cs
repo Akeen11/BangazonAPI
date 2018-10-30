afd55f24bc3bbbc149993a066ad6f11c27f98a70
davidnicholastaylor <dntaylor14@gmail.com>
2018-10-30 11:17:42 -0500

    built model for product

---
+++ Bangazon/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models
+{
+    public class Product
+    {
+    public int Id { get; set; }
+    public double Price { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Quantity { get; set; }
+    public ProductType ProductType { get; set; }
+    public Customer Customer { get; set; }
+    }
+}
